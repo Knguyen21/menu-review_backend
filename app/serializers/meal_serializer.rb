29f6fd328779b3d3fd20d91628902e98a6793e8e
khanhnhat <nguyen.k21@gmail.com>
2016-02-27 15:16:10 -0500

    Update serializer to add foreign keys

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,5 +1,5 @@
 class MealSerializer < ActiveModel::Serializer
-  attributes :id, :meal_type, :name, :desc, :price
+  attributes :id, :meal_type, :name, :desc, :price, :restaurant_id
 
   def price
     "%.2f" % object.price.truncate(2)
