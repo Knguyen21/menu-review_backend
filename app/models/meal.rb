eebe7e2fcba50ffa534edb6cab262f57d7f34ca2
khanhnhat <nguyen.k21@gmail.com>
2016-02-27 14:33:28 -0500

    fix spelling mistake of reviews

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,7 @@
 class Meal < ActiveRecord::Base
   belongs_to :restaurant, inverse_of: :meals
-  has_many :reviews, inverse_of: :meal, foreign_key: 'meal_id'
+  # has_many :reviews, inverse_of: :meal, foreign_key: 'meal_id'
+  has_many :users, through: :reviews
+  has_many :reviews, foreign_key: 'meal_id'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
   #
   class User < ActiveRecord::Base
     include Authentication
-    has_many :reviews, inverse_of: :user
+    has_many :meals, through: :reviews
+    has_many :reviews#, inverse_of: :user
+
   end
