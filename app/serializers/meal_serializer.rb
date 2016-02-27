6e212b839a5bb12762fe26cf7c0df75e46c2d9ff
khanhnhat <nguyen.k21@gmail.com>
2016-02-26 19:59:06 -0500

    convert BigDecimal to a 2 decimal float

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,4 +1,8 @@
 class MealSerializer < ActiveModel::Serializer
   attributes :id, :meal_type, :name, :desc, :price
-  "%.2f" % object.price.truncate(2)
+
+  def price
+    "%.2f" % object.price.truncate(2)
+  end
+
 end
