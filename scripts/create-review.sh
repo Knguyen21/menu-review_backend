39974968d566561042c04167f09f8771ae6d5f7f
khanhnhat <nguyen.k21@gmail.com>
2016-02-27 13:20:32 -0500

    update curl request for posting reviews

--- scripts/create-review.sh
+++ scripts/create-review.sh
@@ -1,8 +1,9 @@
 curl --include --request POST http://localhost:3000/reviews \
+  --header "Authorization: Token token=94b49d60f7f4c469fbe8b44db096e136" \
   --header "Content-Type: application/json" \
   --data '{
-    "credentials": {
-      "email": "kay@me.com",
-      "password": "abc"
+    "review" : {
+    "comment" : "The best thing ever",
+     "rating" : "5"
     }
   }'
