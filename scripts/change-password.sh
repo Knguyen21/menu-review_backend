f996e4a1586639904ee589691c7035b8ea765bc0
khanhnhat <nguyen.k21@gmail.com>
2016-02-27 19:58:01 -0500

    Update password id and token

--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl --include --request PATCH http://localhost:3000/change-password/$ID \
-  --header "Authorization: Token token=$TOKEN" \
+curl --include --request PATCH http://localhost:3000/change-password/1 \
+  --header "Authorization: Token token=4c35ac429924d274781f8c881db1c877" \
   --header "Content-Type: application/json" \
   --data '{
     "passwords": {
