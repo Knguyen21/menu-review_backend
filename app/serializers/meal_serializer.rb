2990c558739f5682ee23456205ecd9cbd710933b
khanhnhat <nguyen.k21@gmail.com>
2016-02-27 21:49:24 -0500

    Add restaurant attribute to meals

--- app/serializers/meal_serializer.rb
+++ app/serializers/meal_serializer.rb
@@ -1,5 +1,6 @@
 class MealSerializer < ActiveModel::Serializer
-  attributes :id, :meal_type, :name, :desc, :price, :restaurant_id
+  attributes :id, :meal_type, :name, :desc, :price, :restaurant_id, :restaurant
+
 
   def price
     "%.2f" % object.price.truncate(2)
