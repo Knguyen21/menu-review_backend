fd50c38e547de827d0dc98e682e86b43bd3f7ec1
khanhnhat <nguyen.k21@gmail.com>
2016-03-01 15:07:56 -0500

    Change token to test backend data

--- scripts/create-review.sh
+++ scripts/create-review.sh
@@ -1,5 +1,5 @@
 curl --include --request POST http://localhost:3000/reviews \
-  --header "Authorization: Token token=94b49d60f7f4c469fbe8b44db096e136" \
+  --header "Authorization: Token token=991e526614170c775cdc4e564317cb00" \
   --header "Content-Type: application/json" \
   --data '{
     "review" : {
